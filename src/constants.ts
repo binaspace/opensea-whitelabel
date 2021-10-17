832880a9201b8b6b51f9957ac79d4e15330de5f0
binaspace <binaspaceprotocol@gmail.com>
2021-10-17 05:42:09 -0700

    initial

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const ASSET_URL =
-  "https://app.nftrade.com/assets/bsc/0xa4f492f6ee02f1c9f6751967c923fdb016dcd273/1";
+  "https://opensea.io/BinaSpace_Proto=?";
 
 export enum EmbedTheme {
   Default = "true",
   Simple = "simple",
 }
 
-export const CHOSEN_THEME = EmbedTheme.Default;
+export const CHOSEN_THEME = EmbedTheme.Simple;
